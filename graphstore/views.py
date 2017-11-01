83213d5314c7f0c4d4720472624fcd98ad0bfcb1
El'endia Starman <lee314b@gmail.com>
2017-11-01 00:27:11 -0400

    Bare-bones HTML copy-pasted from another website.

--- graphstore/views.py
+++ graphstore/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_view(request, **kwargs):
+    context = {}
+
+    print("user:", request.user)
+
+    return render(request, 'graphstore/home.html', context)
